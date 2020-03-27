d0057eb48224962996423040f9e4a8ebb8121a57
flamendless <flamendless8@gmail.com>
2020-03-27 21:01:08 +0800

    Using new motion blur shader; Abstracted moving of camera in relation to MotionBlur

--- src/shaders/motion_blur.glsl
+++ src/shaders/motion_blur.glsl
@@ -1,21 +1,21 @@
-extern int quality;
-extern vec2 canvas_size;
-extern vec2 dir;
-extern float length;
+extern float angle;
+extern float strength;
+const int samples = 25;
+
+vec4 blur(Image texture, vec2 uv, vec2 dir)
+{
+	vec4 l = vec4(0);
+	float delta = 1.0/float(samples);
+	for (float i = 0.0; i < 1.0; i += delta)
+	{
+		l += Texel(texture, uv - vec2(dir.x * i, dir.y * i));
+	}
+	return vec4(l * delta * 0.5);
+}
 
 vec4 effect(vec4 color, Image texture, vec2 tex_coords, vec2 screen_coords)
 {
-	vec4 blur;
-	vec2 c = vec2(0, 0)/canvas_size;
-	vec2 pos = vec2(0.0, 0.0);
-
-	for (float i = 0.0; i < 1.0; i += 1.0/float(quality))
-	{
-		pos.x = (length * dir.x + c.x * dir.x) * i;
-		pos.y = (length * dir.y + c.y * dir.y) * i;
-		blur += Texel(texture, tex_coords - pos);
-	}
-	blur /= float(quality);
-
-	return color * blur;
+	vec2 blur_vec = vec2(cos(radians(angle)), sin(radians(angle))) * strength;
+	vec4 mb = blur(texture, tex_coords, blur_vec);
+	return mb;
 }
