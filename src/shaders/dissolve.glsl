deec8d6f4df940702a49d691722f33753ef1b954
flamendless <flamendless8@gmail.com>
2020-03-28 19:09:50 +0800

    Merged DissolveFlowfield to a general Dissolve class

--- src/shaders/dissolve.glsl
+++ src/shaders/dissolve.glsl
@@ -1,10 +1,10 @@
-extern Image tex_flowfield;
+extern Image tex;
 extern number time;
 
 vec4 effect(vec4 color, Image texture, vec2 tex_coords, vec2 screen_coords)
 {
 	vec4 px = Texel(texture, tex_coords);
-	vec4 px2 = Texel(tex_flowfield, tex_coords);
+	vec4 px2 = Texel(tex, tex_coords);
 	if (px2.r < time)
 		return px * color;
 	else
