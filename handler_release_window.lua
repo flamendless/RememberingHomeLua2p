bcea101e40ed15cabe2014e21fe7858e5f05d3df
flamendless <flamendless8@gmail.com>
2020-04-05 14:10:59 +0800

    Added save filename in luapreprocessor handler

--- handler_release_window.lua
+++ handler_release_window.lua
@@ -21,6 +21,7 @@ _GITHUB_URL_RELEASE = ""
 
 _LOG_OUTPUT = "log_output.txt"
 _LOG_INFO = "info.txt"
+_SAVE_FILENAME = "save_file.lua"
 
 _URL_TWITTER = "https://twitter.com/@flamendless"
 _URL_WEBSITE = "https://flamendless.github.io/goinghomerevisited"
