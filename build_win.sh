aec05ce7a7ec891ce9f5dfeca6bd1a48f02206b4
flamendless <flamendless8@gmail.com>
2020-06-05 17:49:28 +0800

    Added build_win for releasing for windows

---
+++ build_win.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dir_output=output
+file_makelove=makelove.toml
+
+function win32()
+{
+	cd "$dir_output"
+	makelove --config ../"$file_makelove" targets win32
+}
+
+function win64()
+{
+	cd "$dir_output"
+	makelove --config ../"$file_makelove" targets win64
+}
+
+if [ $# -eq 0 ]; then
+	echo "Must pass command: win32 or win64"
+else
+	"$@"
+fi
