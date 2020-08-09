6d13d572b7420466e4f9bd8a84d76166255efc68
flamendless <flamendless8@gmail.com>
2020-08-09 11:49:46 +0800

    Renamed and revised hex to rgb converter script

--- scripts/color.lua
+++ scripts/color.lua
@@ -3,9 +3,13 @@ local function hex_to_rgb(hex)
 	return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
 end
 
+local format = string.format
 local args = {...}
 
 for i, v in ipairs(args) do
 	local r, g, b = hex_to_rgb(v)
-	print(i, v, r/255, g/255, b/255)
+	local _r, _g, _b = r/255, g/255, b/255
+	local str = format("{%f, %f, %f}", _r, _g, _b)
+
+	print(i, v, str)
 end
