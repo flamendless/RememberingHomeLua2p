b6c274fb8930a0b64913f43b2c06d6c114bcf4a9
Charis <conradreyes123@gmail.com>
2020-10-05 14:40:44 +0800

    add: tile sets

---
+++ assets/new_assets/tileset/goingHome_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="goingHome" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="tilesets.png" width="96" height="32"/>
+</tileset>
