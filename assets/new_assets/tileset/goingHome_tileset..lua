7ad6996bfa6fbb611e4096e1eb8beec1803d3bee
Charis <conradreyes123@gmail.com>
2020-10-05 21:04:28 +0800

    add: tile set lua export

---
+++ assets/new_assets/tileset/goingHome_tileset..lua
@@ -0,0 +1,28 @@
+return {
+  version = "1.4",
+  luaversion = "5.1",
+  tiledversion = "1.4.2",
+  name = "goingHome",
+  tilewidth = 32,
+  tileheight = 32,
+  spacing = 0,
+  margin = 0,
+  columns = 18,
+  image = "tilesets.png",
+  imagewidth = 576,
+  imageheight = 32,
+  objectalignment = "unspecified",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 32,
+    height = 32
+  },
+  properties = {},
+  terrains = {},
+  tilecount = 18,
+  tiles = {}
+}
