19c36eaf3096a88fca8adfc8892e3088656f83e7
conrad619 <conradreyes123@gmail.com>
2020-11-06 22:09:12 +0800

    mod: update tiled assets

--- assets/new_assets/tileset/things.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="things" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="../things.png" width="128" height="128"/>
-</tileset>
