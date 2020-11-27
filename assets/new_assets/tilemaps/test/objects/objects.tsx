024f52250184077b2771bea385a77a3218afdb6b
conrad619 <conradreyes123@gmail.com>
2020-11-27 23:05:08 +0800

    mod; push fix

---
+++ assets/new_assets/tilemaps/test/objects/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="objects" tilewidth="96" tileheight="96" tilecount="4" columns="4">
+ <image source="objects.png" width="384" height="96"/>
+</tileset>
---
+++ assets/new_assets/tilemaps/test/tiles/tile_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tile_set" tilewidth="32" tileheight="32" tilecount="24" columns="8">
+ <image source="tilesets_final.png" width="256" height="96"/>
+</tileset>
