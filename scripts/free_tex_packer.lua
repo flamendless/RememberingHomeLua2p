39ca00b3f242f3f34f111e23824e6977a6fd41c2
flamendless <flamendless8@gmail.com>
2021-08-02 12:59:33 +0800

    Implemented Atlas system for packed textures

---
+++ scripts/free_tex_packer.lua
@@ -0,0 +1,47 @@
+-- https://github.com/odrick/free-tex-packer
+-- padding: 1
+-- no extrude
+-- power of two
+-- remove file ext
+local data = {
+	frames = {
+		{{#rects}}
+		{{{name}}} = {
+			frame = {
+				x = {{frame.x}},
+				y = {{frame.y}},
+				w = {{frame.w}},
+				h = {{frame.h}}
+			},
+			rotated = {{rotated}},
+			trimmed = {{trimmed}},
+			spriteSourceSize = {
+				x = {{spriteSourceSize.x}},
+				y = {{spriteSourceSize.y}},
+				w = {{spriteSourceSize.w}},
+				h = {{spriteSourceSize.h}}
+			},
+			sourceSize = {
+				w = {{sourceSize.w}},
+				h = {{sourceSize.h}}
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		}{{^last}},{{/last}}
+		{{/rects}}
+	},
+	meta = {
+		app = "{{{appInfo.url}}}",
+		version = "{{appInfo.version}}",
+		image = "{{config.imageFile}}",
+		format = "{{config.format}}",
+		size = {
+			w = {{config.imageWidth}},
+			h = {{config.imageHeight}}
+		},
+		scale = {{config.scale}}
+	}
+}
+return data
---
+++ src/data/storage_room_items.lua
@@ -0,0 +1,208 @@
+local data = {
+	frames = {
+		shelf = {
+			frame = {
+				x = 1,
+				y = 1,
+				w = 67,
+				h = 55
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 67,
+				h = 55
+			},
+			sourceSize = {
+				w = 67,
+				h = 55
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		iron_horse = {
+			frame = {
+				x = 1,
+				y = 58,
+				w = 30,
+				h = 57
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 30,
+				h = 57
+			},
+			sourceSize = {
+				w = 30,
+				h = 57
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		washing_machine = {
+			frame = {
+				x = 33,
+				y = 58,
+				w = 33,
+				h = 36
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 33,
+				h = 36
+			},
+			sourceSize = {
+				w = 33,
+				h = 36
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		basket = {
+			frame = {
+				x = 70,
+				y = 1,
+				w = 27,
+				h = 27
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 27,
+				h = 27
+			},
+			sourceSize = {
+				w = 27,
+				h = 27
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		mirror = {
+			frame = {
+				x = 70,
+				y = 30,
+				w = 14,
+				h = 21
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 14,
+				h = 21
+			},
+			sourceSize = {
+				w = 14,
+				h = 21
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		bulb = {
+			frame = {
+				x = 33,
+				y = 96,
+				w = 5,
+				h = 18
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 5,
+				h = 18
+			},
+			sourceSize = {
+				w = 5,
+				h = 18
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		wood = {
+			frame = {
+				x = 40,
+				y = 96,
+				w = 17,
+				h = 16
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 17,
+				h = 16
+			},
+			sourceSize = {
+				w = 17,
+				h = 16
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		wood2 = {
+			frame = {
+				x = 59,
+				y = 96,
+				w = 16,
+				h = 17
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 16,
+				h = 17
+			},
+			sourceSize = {
+				w = 16,
+				h = 17
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		}
+	},
+	meta = {
+		app = "http://free-tex-packer.com",
+		version = "0.6.7",
+		image = "storage_room_items.png",
+		format = "RGBA8888",
+		size = {
+			w = 128,
+			h = 128
+		},
+		scale = 1
+	}
+}
+return data
