e501c8a92041332a6ef36c92647338265c253afa
flamendless <flamendless8@gmail.com>
2021-08-17 04:02:45 +0800

    Readded old lighting method for intro scene

--- src/shaders/ngrading_volume_multi.glsl
+++ src/shaders/ngrading_volume_multi.glsl
@@ -2,14 +2,9 @@ extern VolumeImage lut;
 extern VolumeImage lut2;
 extern float dt;
 
-vec4 ngrading(vec4 texCol)
+vec4 ngrading(Image tex, vec2 tc)
 {
 	vec4 texCol = Texel(tex, tc);
 	vec3 colorValue = mix(Texel(lut, texCol.rgb).rgb, Texel(lut2, texCol.rgb).rgb, dt);
-	return vec4(colorValue, texCol.a);
-}
-
-vec4 ngrading(Image tex, vec2 tc)
-{
-	return ngrading(Texel(tex, tc));
+    return vec4(colorValue, texCol.a);
 }
