79541dd68ed54ef552d0a7bfd004215d49f2d959
flamendless <flamendless8@gmail.com>
2021-09-17 14:46:03 +0800

    WIP Kitchen

---
+++ src/data/atlas_kitchen_items.lua
@@ -0,0 +1,521 @@
+local Data = {
+	frames = {
+		tiles = {
+			frame = {
+				x = 4,
+				y = 4,
+				w = 253,
+				h = 27
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 253,
+				h = 27
+			},
+			sourceSize = {
+				w = 253,
+				h = 27
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		table = {
+			frame = {
+				x = 4,
+				y = 39,
+				w = 113,
+				h = 33
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 113,
+				h = 33
+			},
+			sourceSize = {
+				w = 113,
+				h = 33
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		t_cabinet_long = {
+			frame = {
+				x = 125,
+				y = 39,
+				w = 97,
+				h = 29
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 97,
+				h = 29
+			},
+			sourceSize = {
+				w = 97,
+				h = 29
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		b_cabinet_open2 = {
+			frame = {
+				x = 4,
+				y = 80,
+				w = 77,
+				h = 31
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 77,
+				h = 31
+			},
+			sourceSize = {
+				w = 77,
+				h = 31
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		b_cabinet_open1 = {
+			frame = {
+				x = 125,
+				y = 76,
+				w = 75,
+				h = 34
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 75,
+				h = 34
+			},
+			sourceSize = {
+				w = 75,
+				h = 34
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		utility_door = {
+			frame = {
+				x = 4,
+				y = 119,
+				w = 32,
+				h = 68
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 32,
+				h = 68
+			},
+			sourceSize = {
+				w = 32,
+				h = 68
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		left_door = {
+			frame = {
+				x = 44,
+				y = 119,
+				w = 4,
+				h = 65
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 4,
+				h = 65
+			},
+			sourceSize = {
+				w = 4,
+				h = 65
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		right_door = {
+			frame = {
+				x = 56,
+				y = 119,
+				w = 4,
+				h = 65
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 4,
+				h = 65
+			},
+			sourceSize = {
+				w = 4,
+				h = 65
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		ref = {
+			frame = {
+				x = 230,
+				y = 39,
+				w = 25,
+				h = 61
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 25,
+				h = 61
+			},
+			sourceSize = {
+				w = 25,
+				h = 61
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		t_cabinet_multi = {
+			frame = {
+				x = 68,
+				y = 119,
+				w = 58,
+				h = 29
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 58,
+				h = 29
+			},
+			sourceSize = {
+				w = 58,
+				h = 29
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		b_cabinet_single2 = {
+			frame = {
+				x = 68,
+				y = 156,
+				w = 57,
+				h = 30
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 57,
+				h = 30
+			},
+			sourceSize = {
+				w = 57,
+				h = 30
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		t_cabinet_open = {
+			frame = {
+				x = 133,
+				y = 156,
+				w = 57,
+				h = 28
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 57,
+				h = 28
+			},
+			sourceSize = {
+				w = 57,
+				h = 28
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		t_cabinet_right = {
+			frame = {
+				x = 134,
+				y = 118,
+				w = 57,
+				h = 28
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 57,
+				h = 28
+			},
+			sourceSize = {
+				w = 57,
+				h = 28
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		b_cabinet_single1 = {
+			frame = {
+				x = 198,
+				y = 154,
+				w = 55,
+				h = 30
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 55,
+				h = 30
+			},
+			sourceSize = {
+				w = 55,
+				h = 30
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		t_cabinet_single1 = {
+			frame = {
+				x = 208,
+				y = 108,
+				w = 45,
+				h = 29
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 45,
+				h = 29
+			},
+			sourceSize = {
+				w = 45,
+				h = 29
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		stove = {
+			frame = {
+				x = 4,
+				y = 195,
+				w = 42,
+				h = 6
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 42,
+				h = 6
+			},
+			sourceSize = {
+				w = 42,
+				h = 6
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		b_connector = {
+			frame = {
+				x = 89,
+				y = 80,
+				w = 22,
+				h = 30
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 22,
+				h = 30
+			},
+			sourceSize = {
+				w = 22,
+				h = 30
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		t_connector = {
+			frame = {
+				x = 4,
+				y = 209,
+				w = 21,
+				h = 28
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 21,
+				h = 28
+			},
+			sourceSize = {
+				w = 21,
+				h = 28
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		sink = {
+			frame = {
+				x = 33,
+				y = 209,
+				w = 26,
+				h = 11
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 26,
+				h = 11
+			},
+			sourceSize = {
+				w = 26,
+				h = 11
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		stool = {
+			frame = {
+				x = 67,
+				y = 194,
+				w = 19,
+				h = 26
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 19,
+				h = 26
+			},
+			sourceSize = {
+				w = 19,
+				h = 26
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		outlet = {
+			frame = {
+				x = 208,
+				y = 76,
+				w = 14,
+				h = 5
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 14,
+				h = 5
+			},
+			sourceSize = {
+				w = 14,
+				h = 5
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		}
+	},
+	meta = {
+		app = "http://free-tex-packer.com",
+		version = "0.6.7",
+		image = "atlas_kitchen_items.png",
+		format = "RGBA8888",
+		size = {
+			w = 512,
+			h = 256
+		},
+		scale = 1
+	}
+}
+
+return Data
---
+++ src/data/kitchen_items.lua
@@ -0,0 +1,91 @@
+local Data = {
+	{
+		id = "ref",
+		x = 343, y = 67, z = 5,
+	},
+	{
+		id = "left_door",
+		x = 16, y = 63,
+		is_door = true,
+		req_col_dir = -1,
+	},
+	{
+		id = "utility_door",
+		x = 396, y = 60, z = 5,
+		is_door = true,
+	},
+	{
+		id = "right_door",
+		x = 463, y = 63,
+		is_door = true,
+		req_col_dir = 1,
+	},
+
+	{
+		id = "table",
+		x = 164, y = 95, z = 10,
+		no_col = true,
+	},
+
+	{
+		id = "tiles",
+		x = 76, y = 75, z = 1,
+		no_col = true,
+	},
+	{
+		id = "outlet",
+		x = 187, y = 72, z = 5,
+	},
+
+	{
+		id = "b_cabinet_open1",
+		x = 64, y = 94, z = 5,
+	},
+	{
+		id = "b_connector",
+		x = 138, y = 98, z = 3,
+		no_col = true,
+	},
+	{
+		id = "b_cabinet_single1",
+		x = 158, y = 98, z = 3,
+		no_col = true,
+	},
+	{
+		id = "b_cabinet_single2",
+		x = 212, y = 98, z = 3,
+		no_col = true,
+	},
+	{
+		id = "b_cabinet_open2",
+		x = 264, y = 98, z = 5,
+	},
+
+	{
+		id = "t_cabinet_long",
+		x = 64, y = 32, z = 5,
+		no_col = true,
+	},
+	{
+		id = "t_cabinet_open",
+		x = 160, y = 33, z = 5,
+		no_col = true,
+	},
+	{
+		id = "t_cabinet_multi",
+		x = 216, y = 32, z = 5,
+		no_col = true,
+	},
+	{
+		id = "t_cabinet_single1",
+		x = 272, y = 32, z = 5,
+		no_col = true,
+	},
+	{
+		id = "t_cabinet_right",
+		x = 316, y = 33, z = 5,
+		no_col = true,
+	},
+}
+
+return Data
