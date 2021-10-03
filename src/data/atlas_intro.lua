15653c6ac4c61436bdaf3bee18221b1b1db09a50
flamendless <flamendless8@gmail.com>
2021-10-03 13:38:14 +0800

    Greatly improved and optimized Intro scene to use atlas;
    Increased FPS from 12 to 35!

---
+++ src/data/atlas_intro.lua
@@ -0,0 +1,377 @@
+local Data = {
+	frames = {
+		clouds = {
+			frame = {
+				x = 4,
+				y = 4,
+				w = 745,
+				h = 231
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 745,
+				h = 231
+			},
+			sourceSize = {
+				w = 745,
+				h = 231
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		trees_fg = {
+			frame = {
+				x = 4,
+				y = 243,
+				w = 474,
+				h = 91
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 474,
+				h = 91
+			},
+			sourceSize = {
+				w = 474,
+				h = 91
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		buildings = {
+			frame = {
+				x = 4,
+				y = 342,
+				w = 384,
+				h = 59
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 59
+			},
+			sourceSize = {
+				w = 384,
+				h = 59
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		grass = {
+			frame = {
+				x = 4,
+				y = 409,
+				w = 384,
+				h = 31
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 31
+			},
+			sourceSize = {
+				w = 384,
+				h = 31
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		grass2 = {
+			frame = {
+				x = 4,
+				y = 448,
+				w = 384,
+				h = 27
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 27
+			},
+			sourceSize = {
+				w = 384,
+				h = 27
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		grass_back = {
+			frame = {
+				x = 4,
+				y = 483,
+				w = 384,
+				h = 69
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 69
+			},
+			sourceSize = {
+				w = 384,
+				h = 69
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		grass_front = {
+			frame = {
+				x = 4,
+				y = 560,
+				w = 384,
+				h = 59
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 59
+			},
+			sourceSize = {
+				w = 384,
+				h = 59
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		road = {
+			frame = {
+				x = 4,
+				y = 627,
+				w = 384,
+				h = 47
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 47
+			},
+			sourceSize = {
+				w = 384,
+				h = 47
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		trees_bg = {
+			frame = {
+				x = 4,
+				y = 682,
+				w = 384,
+				h = 125
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 384,
+				h = 125
+			},
+			sourceSize = {
+				w = 384,
+				h = 125
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		bg_tree_cover = {
+			frame = {
+				x = 486,
+				y = 243,
+				w = 79,
+				h = 249
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 79,
+				h = 249
+			},
+			sourceSize = {
+				w = 79,
+				h = 249
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		car_headlight = {
+			frame = {
+				x = 396,
+				y = 500,
+				w = 198,
+				h = 99
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 198,
+				h = 99
+			},
+			sourceSize = {
+				w = 198,
+				h = 99
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		post_light = {
+			frame = {
+				x = 602,
+				y = 243,
+				w = 146,
+				h = 154
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 146,
+				h = 154
+			},
+			sourceSize = {
+				w = 146,
+				h = 154
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		post = {
+			frame = {
+				x = 396,
+				y = 342,
+				w = 25,
+				h = 131
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 25,
+				h = 131
+			},
+			sourceSize = {
+				w = 25,
+				h = 131
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		title = {
+			frame = {
+				x = 573,
+				y = 405,
+				w = 118,
+				h = 86
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 118,
+				h = 86
+			},
+			sourceSize = {
+				w = 118,
+				h = 86
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		},
+		title_light = {
+			frame = {
+				x = 602,
+				y = 499,
+				w = 118,
+				h = 86
+			},
+			rotated = false,
+			trimmed = false,
+			spriteSourceSize = {
+				x = 0,
+				y = 0,
+				w = 118,
+				h = 86
+			},
+			sourceSize = {
+				w = 118,
+				h = 86
+			},
+			pivot = {
+				x = 0.5,
+				y = 0.5
+			}
+		}
+	},
+	meta = {
+		app = "http://free-tex-packer.com",
+		version = "0.6.7",
+		image = "atlas_intro.png",
+		format = "RGBA8888",
+		size = {
+			w = 1024,
+			h = 1024
+		},
+		scale = 1
+	}
+}
+
+return Data
