fc95138aef0b3bf415b4285b65950f2281e2b15d
Brandon Blanker Lim-it <flamendless8@gmail.com>
2021-12-30 16:51:41 +0800

    Improved Love dev on Windows

--- build_win.sh
+++ build_win.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-dir_output=output
+dir_source=src
+dir_output=output_dev
 file_makelove=makelove.toml
+cmd="/mnt/c/Windows/System32/cmd.exe"
+path_love="C:\Program Files\LOVE"
+path_game="A:\home\flamendless\GoingHomeRevisited\release\love\GoingHomeRevisited.love"
 
 function win32()
 {
-	cd "$dir_output"
-	makelove --config ../"$file_makelove" win32
+	cd "$dir_output" && makelove --config ../"$file_makelove" win32
 }
 
 function win64()
 {
-	cd "$dir_output"
-	makelove --config ../"$file_makelove" win64
+	cd "$dir_output" && makelove --config ../"$file_makelove" win64
+}
+
+function run()
+{
+	echo "Running build_win.sh"
+	./build.sh process_src "$dir_source"
+	win64
+	$cmd /c "cd $path_love & lovec.exe $path_game"
+	echo "Completed build_win.sh"
 }
 
 if [ $# -eq 0 ]; then
-	echo "Must pass command: win32 or win64"
+	echo "Must pass command: win32, win64, run"
 else
-	if [ ! -d "$dir_output" ]; then
-		mkdir "$dir_output"
-	fi
 	"$@"
 fi
